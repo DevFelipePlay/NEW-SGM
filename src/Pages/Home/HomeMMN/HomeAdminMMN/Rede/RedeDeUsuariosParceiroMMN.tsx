76918b422d76ca033d074a9bc281b9465f1860eb
DevFelipePlay <felipe.mendes@playmovel.com.br>
2023-12-29 15:59:49 -0300

    refatoramento da tela de rede parceiro

--- src/Pages/Home/HomeMMN/HomeAdminMMN/Rede/RedeDeUsuariosParceiroMMN.tsx
+++ src/Pages/Home/HomeMMN/HomeAdminMMN/Rede/RedeDeUsuariosParceiroMMN.tsx
@@ -1,30 +1,16 @@
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import {
-  Box,
-  Button,
-  Pagination,
-  PaginationItem,
-  Stack,
-  Typography,
-} from "@mui/material";
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { mask } from "remask";
-import { postPlayRecuperaNiveisParceiro } from "../../../../../api";
-import { IResPostPlayRecuperaNiveisParceiro } from "../../../../../api/ApisUtils/RecuperaNiveisParceiro/IResPostPlayRecuperaNiveisParceiro";
-import { ListCustom, Loading } from "../../../../../components";
-import useUser from "../../../../../hooks/useUser";
-import { errorToast } from "../../../../../utils";
+import { Box, Paper, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { postPlayRecuperaNiveisParceiro } from '../../../../../api';
+import { IResPostPlayRecuperaNiveisParceiro } from '../../../../../api/ApisUtils/RecuperaNiveisParceiro/IResPostPlayRecuperaNiveisParceiro';
+import { FlexBox, MUIDataTableCustom } from '../../../../../components';
+import useUser from '../../../../../hooks/useUser';
+import { errorToast } from '../../../../../utils';
 
 export default function RedeDeUsuariosParceiroMMN() {
   const [loading, setLoading] = useState(false);
-  const [responseList, setResponseList] = useState<
-    IResPostPlayRecuperaNiveisParceiro[]
-  >([]);
+  const [responseList, setResponseList] = useState<IResPostPlayRecuperaNiveisParceiro[]>([]);
   const { user } = useUser();
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 5;
 
   const navigate = useNavigate();
 
@@ -32,7 +18,7 @@ export default function RedeDeUsuariosParceiroMMN() {
     setLoading(true);
 
     let payload = {
-      cpf: user?.cpf ? user?.cpf : "",
+      cpf: user?.cpf ? user?.cpf : '',
     };
     try {
       const data = await postPlayRecuperaNiveisParceiro(payload);
@@ -44,141 +30,65 @@ export default function RedeDeUsuariosParceiroMMN() {
     }
   }
 
-  const handlePageChange = (
-    //@ts-ignore
-    event: React.ChangeEvent<unknown>,
-    page: number
-  ) => {
-    setCurrentPage(page);
-  };
-
-  const renderPaginatedList = () => {
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    const paginatedItems = responseList.slice(startIndex, endIndex);
-
-    return paginatedItems.map((item, index) => (
-      <Box sx={{ width: "100%" }} key={index}>
-        <ListCustom
-          avatar={""}
-          nome={item.nome}
-          cpf={mask(item.cpf, ["999.999.999-99", "99.999.999/9999-99"])}
-          editar={() => ""}
-          excluir={() => ""}
-          pressItemList={() =>
-            navigate(`/daashboard-relatorio-usuario-mmn/${item.cpf}`)
-          }
-        />
-      </Box>
-    ));
-  };
-
   useEffect(() => {
     handleListRedeDeUsuarioMMN();
     console.log(responseList);
   }, []);
 
   return (
-    <>
-      {responseList.length === 0 ? (
-        <>
-          <Box
+    <Box sx={{ width: '100%' }}>
+      <FlexBox sx={{ gap: 2 }}>
+        <Paper elevation={3} sx={{ mt: 2, p: 2 }}>
+          <FlexBox
             sx={{
-              width: "100%",
-              m: 1,
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
+              flexDirection: 'row',
+              alignItems: 'center',
+              textAlign: {
+                xs: 'center',
+                sm: 'initial',
+              },
             }}
           >
-            <Typography variant="h4">Rede de usuários</Typography>
-            <Button
-              onClick={() => navigate("/cadastro-usuario")}
-              variant="contained"
-            >
-              Cadastrar
-            </Button>
-          </Box>
-          <Typography variant="h4" sx={{ mt: 10 }}>
-            Sem usuários cadastrados
-          </Typography>
-        </>
-      ) : (
-        <>
-          {loading ? (
-            <Box
-              sx={{
-                width: "100%",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                height: "50vh",
-              }}
-            >
-              <Loading />
-            </Box>
-          ) : (
-            <Box
-              sx={{
-                mb: 2,
-                width: "100%",
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                flexDirection: "column",
-              }}
-            >
-              <Box
-                sx={{
-                  width: "100%",
-                  m: 1,
-                  display: "flex",
-                  justifyContent: "space-between",
-                  alignItems: "center",
-                  flexDirection: {
-                    xs: "column",
-                    sm: "row",
-                  },
-                  textAlign: {
-                    xs: "center",
-                    sm: "inherit",
-                  },
-                  letterSpacing: {
-                    xs: "0",
-                    sm: "inherit",
-                  },
-                }}
-              >
-                <Typography variant="h4">Rede de usuários</Typography>
-                <Button
-                  onClick={() => navigate("/cadastro-usuario")}
-                  variant="contained"
-                >
-                  Cadastrar
-                </Button>
-              </Box>
-              {renderPaginatedList()}
-
-              <Stack spacing={2}>
-                <Pagination
-                  count={Math.ceil(responseList.length / itemsPerPage)}
-                  page={currentPage}
-                  onChange={handlePageChange}
-                  renderItem={(item) => (
-                    <PaginationItem
-                      slots={{
-                        previous: ArrowBackIcon,
-                        next: ArrowForwardIcon,
-                      }}
-                      {...item}
-                    />
-                  )}
-                />
-              </Stack>
-            </Box>
-          )}
-        </>
-      )}
-    </>
+            <Typography variant='h4' gutterBottom flexGrow={1} sx={{ mt: 2 }}>
+              Usuários
+            </Typography>
+          </FlexBox>
+          <MUIDataTableCustom
+            title=''
+            data={responseList}
+            options={{
+              // @ts-ignore
+              onRowClick: (d, { dataIndex }) => {
+                responseList[dataIndex].cpf;
+                navigate(`/daashboard-relatorio-usuario-mmn/${responseList[dataIndex].cpf}`);
+              },
+            }}
+            loading={loading}
+            columns={[
+              {
+                name: 'nome',
+                label: 'Nome',
+              },
+              {
+                name: 'cpf',
+                label: 'CPF',
+              },
+              {
+                name: 'graduacao',
+                label: 'Graduação',
+                options: {
+                  customBodyRender: (value) => (
+                    <Box sx={{ textAlign: 'center' }}>
+                      {' '}
+                      {value === null ? 'Sem Graduação' : value}
+                    </Box>
+                  ),
+                },
+              },
+            ]}
+          />
+        </Paper>
+      </FlexBox>
+    </Box>
   );
 }
